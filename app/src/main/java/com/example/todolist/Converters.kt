38117fbfdaa3fae76cc0a34ea94d1ab9fe100250
MatthewLeigh <matthewcross97@gmail.com>
2025-01-29 10:02:19 +1100

    Add converters for DateTime

--- app/src/main/java/com/example/todolist/Converters.kt
+++ app/src/main/java/com/example/todolist/Converters.kt
@@ -1,8 +1,22 @@
 package com.example.todolist
 
 import androidx.room.TypeConverter
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class Converters {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+    @TypeConverter
+    fun fromLocalDateToString(date: LocalDate?): String? {
+        return date?.format(formatter)
+    }
+
+    @TypeConverter
+    fun toStringToLocalDate(dateString: String?): LocalDate? {
+        return dateString?.let { LocalDate.parse(it, formatter) }
+    }
+
     @TypeConverter
     fun fromFrequencyToString(frequency: Frequency): String {
         return frequency.name
