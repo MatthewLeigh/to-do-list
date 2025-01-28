09a344ceb6cf76f1da910b6515416c84fa9ff373
MatthewLeigh <matthewcross97@gmail.com>
2025-01-29 09:17:17 +1100

    Add remaining functions from task dao to task repository

--- app/src/main/java/com/example/todolist/TaskRepository.kt
+++ app/src/main/java/com/example/todolist/TaskRepository.kt
@@ -4,17 +4,22 @@ import androidx.lifecycle.LiveData
 
 class TaskRepository(private val taskDao: TaskDao) {
 
-    val allTasks: LiveData<List<Task>> = taskDao.getAllTasks()
-
     suspend fun insert(task: Task) {
         taskDao.insert(task)
+    }
+
+    suspend fun update(task: Task) {
+        taskDao.update(task)
     }
 
     suspend fun delete(task: Task) {
         taskDao.delete(task)
     }
 
-    suspend fun update(task: Task) {
-        taskDao.update(task)
-    }
+    val getAllTasks: LiveData<List<Task>> = taskDao.getAllTasks()
+
+    fun getTaskById(taskId: Int): LiveData<Task> = taskDao.getTaskById(taskId)
+
+    fun getTasksByFrequency(frequency: Frequency): LiveData<List<Task>> = taskDao.getTasksByFrequency(frequency)
+
 }
