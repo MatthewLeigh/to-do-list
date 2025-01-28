2fd1920fd01161c75d0bc86ef8373a6bc41ae4f0
MatthewLeigh <matthewcross97@gmail.com>
2025-01-29 09:38:27 +1100

    Rename from TaskDatabase to AppDatabase since it holds both Task and TaskInstance tables

--- app/src/main/java/com/example/todolist/AppDatabase.kt
+++ app/src/main/java/com/example/todolist/AppDatabase.kt
@@ -5,23 +5,26 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.Room
 import androidx.room.TypeConverters
+import com.example.todolist.Task.Task
+import com.example.todolist.Task.TaskDao
+import com.example.todolist.TaskInstance.TaskInstanceDao
 
 @Database(entities = [Task::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
-abstract class TaskDatabase : RoomDatabase() {
+abstract class AppDatabase : RoomDatabase() {
     abstract fun getTasksDao(): TaskDao
     abstract fun getTaskInstanceDao(): TaskInstanceDao
 
     companion object{
 
         @Volatile
-        private var INSTANCE: TaskDatabase? = null
+        private var INSTANCE: AppDatabase? = null
 
-        fun getDatabase(context: Context): TaskDatabase{
+        fun getDatabase(context: Context): AppDatabase{
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
-                    TaskDatabase::class.java,
+                    AppDatabase::class.java,
                     "task_database"
                 ).build()
                 INSTANCE = instance
