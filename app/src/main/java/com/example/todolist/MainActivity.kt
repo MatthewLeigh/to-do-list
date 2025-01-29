f7cd7d480b935ccf2def486416f09dfaefe855d5
MatthewLeigh <matthewcross97@gmail.com>
2025-01-29 12:01:44 +1100

    Integrate adapter into main

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.todolist
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.task.Task
+import com.example.todolist.task.TaskAdapterMain
+import com.example.todolist.task.TaskViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TaskAdapterMain.TaskClickDeleteInterface, TaskAdapterMain.TaskClickUpdateInterface {
+
+    lateinit var taskList : RecyclerView
+    lateinit var addButton : FloatingActionButton
+    lateinit var taskViewModel : TaskViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
@@ -16,5 +31,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        taskList = findViewById(R.id.MainTaskListRV)
+        addButton = findViewById(R.id.MainAddButton)
+        taskList.layoutManager = LinearLayoutManager(this)
+
+        val taskAdapterMain = TaskAdapterMain(this, this, this)
+        taskViewModel = ViewModelProvider(this, ViewModelProvider.AndroidViewModelFactory.getInstance(application))[taskViewModel::class.java]
+        taskViewModel.allTasks.observe(this, { list->
+            list?.let {
+                taskAdapterMain.updateList(it)
+            }
+        })
+
+        addButton.setOnClickListener {
+            val intent = Intent(this@MainActivity, CreateOrUpdateTaskActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+
+    }
+
+    override fun onDeleteButtonClick(task: Task) {
+        taskViewModel.deleteTask(task)
+        Toast.makeText(
+            this,
+            "${task.taskTitle} Deleted",
+            Toast.LENGTH_LONG
+        ).show()
+    }
+
+    override fun onTaskItemClick(task: Task) {
+        val intent = Intent(this@MainActivity, CreateOrUpdateTaskActivity::class.java)
+        intent.putExtra("taskType", "Update")
+        intent.putExtra("taskTitle", task.taskTitle)
+        intent.putExtra("taskDescription", task.taskDescription)
+        startActivity(intent)
+        this.finish()
     }
 }
