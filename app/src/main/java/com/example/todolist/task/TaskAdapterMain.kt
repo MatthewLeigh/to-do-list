9ed1fb56b404140d478053e0697456c8a0a765c8
MatthewLeigh <matthewcross97@gmail.com>
2025-01-29 14:10:37 +1100

    Change how views are accessed

--- app/src/main/java/com/example/todolist/task/TaskAdapterMain.kt
+++ app/src/main/java/com/example/todolist/task/TaskAdapterMain.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todolist.R
@@ -17,19 +18,20 @@ class TaskAdapterMain(
     private val allTasks = ArrayList<Task>()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val listTaskTitle = itemView.findViewById<TextView>(R.id.listTaskTitle)
-        val listTaskSubtitle = itemView.findViewById<TextView>(R.id.listTaskSubtitle)
-        val listTaskDeleteButton = itemView.findViewById<TextView>(R.id.listTaskDeleteButton)
+        val listTaskTitle: TextView = itemView.findViewById(R.id.listTaskTitle)
+        val listTaskSubtitle: TextView = itemView.findViewById(R.id.listTaskSubtitle)
+        val listTaskDeleteButton: ImageView = itemView.findViewById(R.id.listTaskDeleteButton)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.component_list_task_item, parent, false)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = LayoutInflater.from(parent.context)
+            .inflate(R.layout.component_list_task_item, parent, false)
         return ViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.listTaskTitle.text = allTasks[position].taskTitle
-        holder.listTaskSubtitle.text = allTasks[position].taskCategory
+        holder.listTaskSubtitle.text = allTasks[position].taskId.toString()
 
         holder.listTaskDeleteButton.setOnClickListener {
             taskClickDeleteInterface.onDeleteButtonClick(allTasks[position])
@@ -44,16 +46,18 @@ class TaskAdapterMain(
         return allTasks.size
     }
 
-    fun updateList(newList : List<Task>) {
+    fun updateList(newList: List<Task>) {
         allTasks.clear()
         allTasks.addAll(newList)
         notifyDataSetChanged()
-}
+    }
 
-interface TaskClickDeleteInterface {
-    fun onDeleteButtonClick(task: Task)
-}
+    interface TaskClickDeleteInterface {
+        fun onDeleteButtonClick(task: Task)
+    }
 
-interface TaskClickUpdateInterface {
-    fun onTaskItemClick(task: Task)
+    interface TaskClickUpdateInterface {
+        fun onTaskItemClick(task: Task)
+    }
+    
 }
